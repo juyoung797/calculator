a8828d2da6e87218ea61fb67184835c854973c04
juyoung <juyoungjeong.797@gmail.com>
2025-06-26 15:29:29 +0900

    Initial commit

---
+++ src/main/java/com/j797/calculator/service/CalculatorService.java
@@ -0,0 +1,25 @@
+package com.j797.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public double calculate(double num1, double num2, String operation) {
+        switch (operation) {
+            case "add":
+                return num1 + num2;
+            case "subtract":
+                return num1 - num2;
+            case "multiply":
+                return num1 * num2;
+            case "divide":
+                if (num2 == 0) {
+                    throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+                } else {
+                    return num1 / num2;
+                }
+            default:
+                throw new IllegalArgumentException("허용되지 않은 연산자입니다." + operation);
+        }
+    }
+}
